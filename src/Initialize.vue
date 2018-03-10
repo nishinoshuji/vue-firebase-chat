cee64cc822d8f60aa42bac361561210e7f70ca83
‘surumebeer’ <06shuji09@gmail.com>
2018-03-11 04:27:18 +0900

    fix

---
+++ src/Initialize.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button @click="init">Init</button>
+    <button @click="get">Get</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'initializeData',
+  methods: {
+    init() {
+      this.$store.dispath('users/seed')
+      this.$store.dispath('conversations/seed')
+    }
+    get() {
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
